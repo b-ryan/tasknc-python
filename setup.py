2cd2647e84c074c6cc4eb3f0905cd2cf3b5d7cbe
Buck Ryan <buck.ryan2@gmail.com>
2018-01-29 13:12:28 -0500

    0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="pytasknc",
-    version="0.1.0",
+    version="0.1.1",
     description="NCurses for TaskWarrior",
     author="Buck Ryan",
     url="https://github.com/b-ryan/tasknc-python",
